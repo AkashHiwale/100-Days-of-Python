b65155d8f9c5245e2ac7c0b1e474115409e6c2da
AkashHiwale <64581638+AkashHiwale@users.noreply.github.com>
2021-07-07 18:16:44 +0530

    Create Day 7: Hangman Game.py

---
+++ Day 7: Hangman Game.py
@@ -0,0 +1,61 @@
+Play here - https://replit.com/@AkashHiwale/Day-7-Hangman-5-Start#main.py
+
+from replit import clear
+import random
+import hangman_art
+import hangman_words
+
+
+print(hangman_art.logo)
+
+word_list = hangman_words.word_list
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    clear()
+
+    if guess in display:
+      print(f"{guess} has been already gussed.")
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+
+        print(f"You have guessed a wrong letter. Lives remaining {lives}")
+
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+            print(f"The correct letter was {chosen_word}.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    
+    print(hangman_art.stages[lives])
